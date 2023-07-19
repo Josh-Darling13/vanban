c39c6c9fc423d645920eacba53b55b36f9b8fc24
Josh-Darling13 <squiddarling@gmail.com>
2023-07-18 22:15:58 -0400

    created localstorage read function

--- js/api/KanbanAPI.js
+++ js/api/KanbanAPI.js
@@ -1,5 +1,30 @@
 // Static methods to interact with local storage
 
 export default class KanbanAPI {
-    
+
+}
+
+function read () {
+
+    // creates a read function with ether empty valuse if this si the users first visit
+    // or parses values if return visit
+    const json = localStorage.getItem("kanban-data");
+    if(!json){
+        return[
+            {
+                id: 1,
+                items:[]
+            },
+            {
+                id: 2,
+                items:[]
+            },
+            {
+                id: 3,
+                items:[]
+            },           
+        ]
+    }
+
+    return JSON.parse(json);
 }
